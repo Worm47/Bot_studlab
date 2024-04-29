8880c9535b41e5fd8c2e3d2ed6b4a1274ded9cce
Worm47 <159001839+Worm47@users.noreply.github.com>
2024-04-29 23:00:18 +0600

    Update config.py

--- configuration/config.py
+++ configuration/config.py
@@ -2,4 +2,4 @@ TOKEN_TG="6656172382:AAFYlIz6YahL42y6RCH5-L5cWjDIMzAwrQE"
 API_KEY_WEATHER="738df5567dff4be9992122801242504"
 API_KEY_NEWS="76d11f1beef4401f9f66746533d9dd3a"
 
-# Необдходимые библиотеки: aiogram, sqlalchemy, aiosqlite
+# Необдходимые библиотеки: aiogram, sqlalchemy, aiosqlite, requests
